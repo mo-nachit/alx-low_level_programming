3dd6221e8d474afbfc5a8c0c2e7b05704f1d5a49
monachit <moadnachit6@gmail.com>
2023-03-17 11:10:28 -0700

    moaoaoa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+}
