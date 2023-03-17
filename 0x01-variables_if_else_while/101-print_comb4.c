a135f562726a189d1f40206c5ed33bdcc688b0ae
monachit <moadnachit6@gmail.com>
2023-03-17 15:06:15 -0700

    masmsms

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (a > b && c > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a < 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
