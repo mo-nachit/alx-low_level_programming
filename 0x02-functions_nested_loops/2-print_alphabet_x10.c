017ee58cc71e09d4a3c595156559e481ef8a16a1
monachit <moadnachit6@gmail.com>
2023-03-21 13:11:45 -0700

    mamam

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,9 @@
 #include "main.h"
+
+
 /**
-* print_alphabet - print all alphabet in lowercase
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
 */
 void print_alphabet_x10(void)
 {
