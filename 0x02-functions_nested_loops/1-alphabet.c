4778ed0ce6cb390505131ca93ce48865223c80e9
monachit <moadnachit6@gmail.com>
2023-03-21 12:29:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97 ; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
