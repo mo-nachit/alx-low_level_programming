0c8b729e82e5f4c6a42d2dd761ee8321d9668fae
monachit <moadnachit6@gmail.com>
2023-03-21 05:43:17 -0700

    nicd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
