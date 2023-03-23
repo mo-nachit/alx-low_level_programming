186cbd8c589be5c341a158582bfb1e8a2cc1e83e
monachit <moadnachit6@gmail.com>
2023-03-23 11:42:28 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+		}
+		putchar('\n');
+	}
+}
