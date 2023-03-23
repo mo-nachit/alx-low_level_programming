4241e035ab6f5bae44cfe15e0ab6728c4c2457d3
monachit <moadnachit6@gmail.com>
2023-03-23 08:07:02 -0700

    moad

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
