66634206c48fa86d1521a2e98b7808cb64a53f0a
monachit <moadnachit6@gmail.com>
2023-04-09 05:23:47 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
-			i	return (1);
+				return (1);
 			}
 		}
 	       r += atoi(argv[i]);
